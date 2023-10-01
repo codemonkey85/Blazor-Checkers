c43709be4c36b1fd274145abc7a76d2c62aea266
Michael Bond <michael@bondcodes.com>
2023-10-01 18:49:39 -0400

    Set title, use static methods

--- Checkers/Components/Board.razor.cs
+++ Checkers/Components/Board.razor.cs
@@ -6,13 +6,22 @@ public partial class Board
 
     private PieceType GetPiece(int row, int col) => pieces[row, col];
 
-    private string GetPieceClass(int row, int col) => GetPiece(row, col) switch
+    private static string GetPieceClass(PieceType pieceType) => pieceType switch
     {
         PieceType.Red => "red",
         PieceType.RedKing => "red king",
         PieceType.Black => "black",
         PieceType.BlackKing => "black king",
         _ => "empty"
+    };
+
+    private static string GetTileTitle(PieceType pieceType) => pieceType switch
+    {
+        PieceType.Red => "Red Piece",
+        PieceType.RedKing => "Red King",
+        PieceType.Black => "Black Piece",
+        PieceType.BlackKing => "Black King",
+        _ => "Empty Tile"
     };
 
     private void SetPiece(int row, int col, PieceType piece) => pieces[row, col] = piece;
@@ -45,14 +54,14 @@ public partial class Board
 
     }
 
-    private (int Row, int Col)[] DefaultBlackPositions = new (int Row, int Col)[]
+    private readonly (int Row, int Col)[] DefaultBlackPositions = new (int Row, int Col)[]
     {
         (0, 1), (0, 3), (0, 5), (0, 7),
         (1, 0), (1, 2), (1, 4), (1, 6),
         (2, 1), (2, 3), (2, 5), (2, 7)
     };
 
-    private (int Row, int Col)[] DefaultRedPositions = new (int Row, int Col)[]
+    private readonly (int Row, int Col)[] DefaultRedPositions = new (int Row, int Col)[]
     {
         (5, 0), (5, 2), (5, 4), (5, 6),
         (6, 1), (6, 3), (6, 5), (6, 7),
